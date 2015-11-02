9c592b9b8bafdd10a83e94c0148bab395a7c9b48
Kurt Kiefer <kekiefer@gmail.com>
2015-11-01 16:03:23 -0800

    Fixed external oscillator value for new board

--- Inc/stm32f4xx_hal_conf.h
+++ Inc/stm32f4xx_hal_conf.h
@@ -99,7 +99,7 @@
   *        (when HSE is used as system clock source, directly or through the PLL).  
   */
 #if !defined  (HSE_VALUE) 
-  #define HSE_VALUE    ((uint32_t)14318182*2) /*!< Value of the External oscillator in Hz */
+  #define HSE_VALUE    ((uint32_t)14318182) /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 #if !defined  (HSE_STARTUP_TIMEOUT)
