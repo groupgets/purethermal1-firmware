ba4aae34146af58d4254ab51de617112ef982394
Kurt Kiefer <kekiefer@gmail.com>
2016-02-17 16:06:26 -0800

    Generalize image upscaling in lcd_display_task

--- Src/lcd_display_task.c
+++ Src/lcd_display_task.c
@@ -7,7 +7,14 @@
 #include "lcd_display.h"
 #include "project_config.h"
 
-extern display16_t display_buffer[160*128];
+extern display16_t display_buffer[];
+
+#define UPSCALING_X   ( LCD_WIDTH / FRAME_LINE_LENGTH )
+#define UPSCALING_Y   ( LCD_HEIGHT / IMAGE_NUM_LINES )
+#define FRAME_WIDTH   ( FRAME_LINE_LENGTH * UPSCALING_X )
+#define FRAME_HEIGHT  ( IMAGE_NUM_LINES * UPSCALING_Y )
+#define X_OFFSET      ( (LCD_WIDTH - FRAME_WIDTH) / 2 )
+#define Y_OFFSET      ( (LCD_HEIGHT - FRAME_HEIGHT) / 2 )
 
 static yuv422_buffer_t *last_buffer;
 static lepton_buffer *last_buffer_rgb;
@@ -15,7 +22,7 @@ static lepton_buffer *last_buffer_rgb;
 PT_THREAD( lcd_display_task(struct pt *pt))
 {
   static int last_frame_count = 0;
-  static int i,j;
+  static int i,j,k,l;
   static rgb_t rgb;
   static display16_t val;
 
@@ -34,16 +41,18 @@ PT_THREAD( lcd_display_task(struct pt *pt))
         rgb = last_buffer_rgb->lines[IMAGE_OFFSET_LINES + j].data.image_data[i];
         val = rgb_to_packed(rgb.r, rgb.g, rgb.b);
 
-        display_buffer[j * 2 * LCD_WIDTH + i * 2 + 0] = val;
-        display_buffer[j * 2 * LCD_WIDTH + i * 2 + 1] = val;
-
-        display_buffer[(j * 2 + 1) * LCD_WIDTH + i * 2 + 0] = val;
-        display_buffer[(j * 2 + 1) * LCD_WIDTH + i * 2 + 1] = val;
+        for (k = 0; k < UPSCALING_Y; k++)
+        {
+          for (l = 0; l < UPSCALING_X; l++)
+          {
+            display_buffer[(j * UPSCALING_Y + k) * FRAME_WIDTH + i * UPSCALING_X + l] = val;
+          }
+        }
       }
     }
 
-    lcd_set_mem_window(0, 4, LCD_WIDTH-1, LCD_HEIGHT-4-1);
-    lcd_blit_async(display_buffer, LCD_WIDTH, LCD_HEIGHT-8);
+    lcd_set_mem_window(X_OFFSET, Y_OFFSET, X_OFFSET+FRAME_WIDTH-1, Y_OFFSET+FRAME_HEIGHT-1);
+    lcd_blit_async(display_buffer, FRAME_WIDTH, FRAME_HEIGHT);
   }
   PT_END(pt);
 }
