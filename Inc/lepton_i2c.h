9674c56f6d64cf4eb54d678258cdf40bdd57b301
Sheyne Anderson <sanderson@novacoast.com>
2019-02-15 14:52:24 -0800

    simplify state machine

--- Inc/lepton_i2c.h
+++ Inc/lepton_i2c.h
@@ -13,7 +13,7 @@ HAL_StatusTypeDef lepton_read_data(uint8_t * data);
 HAL_StatusTypeDef init_lepton_command_interface(void);
 HAL_StatusTypeDef disable_lepton_agc();
 HAL_StatusTypeDef enable_lepton_agc();
-HAL_StatusTypeDef restore_cached_values();
+HAL_StatusTypeDef disable_rgb888();
 HAL_StatusTypeDef disable_telemetry(void);
 HAL_StatusTypeDef enable_telemetry(void);
 HAL_StatusTypeDef get_scene_stats(uint16_t *min, uint16_t *max, uint16_t *avg);
